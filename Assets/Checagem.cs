2e5c273d46379702df48d13f45b4b0362f595388
Laryssa Maciel <101884554+MiguelxD14@users.noreply.github.com>
2023-06-17 18:06:43 -0300

    Mecânicas core finalizadas
    Mecânicas core englobam todas as exigências do teste.

--- Assets/Checagem.cs
+++ Assets/Checagem.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Checagem : MonoBehaviour
 {
@@ -8,20 +9,26 @@ public class Checagem : MonoBehaviour
 
     public Sanduiches[] SanduicheConfig; // Armazena os tipos de sanduiche existentes
     public string[] Ingredientes; // Armazena os ingredientes selecionados;
-    public GameObject SanduichePedido; // Pega o gameObj responsavel por dizer qual sanduiche deve ser pronto;
+    public GameObject SanduichePedido, pontuacao,Resultados; // SanduichePedido: Pega o gameObj responsavel por dizer qual sanduiche deve ser pronto; pontuacao: Acessa o GameObj de pontuação; Resultados: acessa a tela final de resultados;
+    public tempoDecorrido TempoAtual; 
     public int Pontos; // atribui pontuação caso o jogador acerte;
     public bool Acertou = false, Errou = false; // Checa se o jogador acertou alguma combinação
     public int Acertos, Erros, Perdas; // Quantitativo de Acertos, Erros e Perdas (Perdas se tratam das vezes em que o jogador não entregou nenhum pedido dentro dex segundos)
     void Start()
     {
         SanduichePedido = GameObject.FindGameObjectWithTag("Sanduiche");
+        pontuacao = GameObject.FindGameObjectWithTag("Ganho");
+        TempoAtual = GameObject.FindGameObjectWithTag("Tempo").GetComponent<tempoDecorrido>();
         NovoSanduiche();
     }
     void Update()
     {
+        ResultadosFinais();
         if(Acertou == true)
         {
             Acertos += 1;
+            Pontos += 20;
+            pontuacao.GetComponent<TextMeshProUGUI>().text = "R$ " + Pontos.ToString();
             NovoSanduiche();
             Acertou = false;
             
@@ -30,12 +37,26 @@ public class Checagem : MonoBehaviour
         else if(Errou == true)
         {
             Erros += 1;
+            Pontos -= 20;
+            pontuacao.GetComponent<TextMeshProUGUI>().text = "R$ " + Pontos.ToString();
             NovoSanduiche();
             Errou = false;
         }
 
     }
 
+    public void ResultadosFinais()
+    {
+        if(TempoAtual.Tempo == 0)
+        {   
+        Resultados.SetActive(true);
+        Resultados.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = "Sanduiches Vendidos: " + Acertos.ToString();
+        Resultados.transform.GetChild(2).GetComponent<TextMeshProUGUI>().text = "Sanduiches Errados: " + Erros.ToString();
+        Resultados.transform.GetChild(3).GetComponent<TextMeshProUGUI>().text = "Sanduiches Perdidos: " + Perdas.ToString();
+        Resultados.transform.GetChild(4).GetComponent<TextMeshProUGUI>().text = "Lucro do dia: R$ " + Pontos.ToString();
+        }
+        
+    }
     public void AdcionarIngredientes(GameObject Op)
     {
        
---
+++ Assets/ResetarCena.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetarCena : MonoBehaviour
+{
+    //Reseta a cena atual
+    public void OnClick()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
+    }
+}
