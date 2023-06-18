62485dc9bed17f18dcb546605935ca711d921bf5
Laryssa Maciel <101884554+MiguelxD14@users.noreply.github.com>
2023-06-18 12:12:41 -0300

    Adição de novos sprites e feedbacks sonoros
    Feedbacks sonoros para acerto, erro, seleção de botões, musica de fundo e tela final. Sprites novos para mais 5 tipos de sanduiche.

--- Assets/Checagem.cs
+++ Assets/Checagem.cs
@@ -10,8 +10,9 @@ public class Checagem : MonoBehaviour
 
     public Sanduiches[] SanduicheConfig; // Armazena os tipos de sanduiche existentes
     public string[] Ingredientes; // Armazena os ingredientes selecionados;
+    public AudioClip[] Sons; // Gerencia os sons de acerto e erro;
     public GameObject SanduichePedido,pontuacao,Resultados,Cliente; // SanduichePedido: Pega o gameObj responsavel por dizer qual sanduiche deve ser pronto; pontuacao: Acessa o GameObj de pontuação; Resultados: acessa a tela final de resultados; Cliente: gameobject responsavel por controlar a mudança de estados do cliente
-    public tempoDecorrido TempoAtual; 
+    public tempoDecorrido TempoAtual; // Acessa o script que controla o tempo decorrido;
     public int Pontos; // atribui pontuação caso o jogador acerte;
     public bool Acertou = false, Errou = false; // Checa se o jogador acertou alguma combinação
     public int Acertos, Erros, Perdas; // Quantitativo de Acertos, Erros e Perdas (Perdas se tratam das vezes em que o jogador não entregou nenhum pedido dentro dex segundos)
@@ -30,6 +31,8 @@ public class Checagem : MonoBehaviour
         {
             Acertos += 1;
             Pontos += 20;
+            this.GetComponent<AudioSource>().clip = Sons[0];
+            this.GetComponent<AudioSource>().Play();
             pontuacao.GetComponent<TextMeshProUGUI>().text = "R$ " + Pontos.ToString();
             NovoSanduiche();
             Acertou = false;
@@ -40,6 +43,8 @@ public class Checagem : MonoBehaviour
         {
             Erros += 1;
             Pontos -= 20;
+            this.GetComponent<AudioSource>().clip = Sons[1];
+            this.GetComponent<AudioSource>().Play();
             pontuacao.GetComponent<TextMeshProUGUI>().text = "R$ " + Pontos.ToString();
             NovoSanduiche();
             Errou = false;
